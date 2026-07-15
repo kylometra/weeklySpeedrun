1c493054ea914e75b427e240d645e2d3e72d20f7
metra <metraberryy@gmail.com>
2026-07-14 22:51:53 -0400

    reverting db path change + debugging message

--- WeeklyIL/Database/WilDbContext.cs
+++ WeeklyIL/Database/WilDbContext.cs
@@ -9,7 +9,15 @@ public class WilDbContext : DbContext
     public DbSet<WeekEntity> Weeks { get; set; }
     public DbSet<ScoreEntity> Scores { get; set; }
     
-    public string DbPath { get; } = "WilBot.db";
+    public string DbPath { get; }
+    
+    public WilDbContext()
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create);
+        DbPath = Path.Join(path, "WilBot/WilBot.db");
+        
+        if (!File.Exists(DbPath)) Console.WriteLine($"DB doesn't exist at \"{DbPath}\" yet!");
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");
